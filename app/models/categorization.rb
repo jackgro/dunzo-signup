669e049aded0b588b8ad008333bbab6702eb69b7
Jack Grossmann <jtg028@gmail.com>
2012-11-17 01:00:24 -0500

    added proper has_many :through associations

--- app/models/categorization.rb
+++ app/models/categorization.rb
@@ -1,3 +1,5 @@
 class Categorization < ActiveRecord::Base
   attr_accessible :name, :task_id, :user_id
+  belongs_to :users
+  belongs_to :tasks
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ActiveRecord::Base
   attr_accessible :name
+
+  has_many :categorizations
+  has_many :users, :through => :categorizations
 end
