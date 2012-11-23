64dd5b44917810226ea7b60c52e0e9723032a16e
Jack Grossmann <jtg028@gmail.com>
2012-11-22 20:34:42 -0500

    task create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,14 @@
 class TasksController < ApplicationController
   def new
   end
+
+  def create
+  	@category = Category.find(params[:category_id])
+  	@task = @category.tasks.create(params[:task])
+  	if @task.save
+  		redirect_to :back, notice: "Task created."
+  	else
+  		# render
+  	end
+  end
 end
