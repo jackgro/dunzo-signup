4922362ed3e01f4392ac98b6dd2da7f0c0742d69
Jack Grossmann <jtg028@gmail.com>
2012-11-22 20:36:24 -0500

    added routes for categories and tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RailsPrelaunchSignup::Application.routes.draw do
+
+
   authenticated :user do
     root :to => 'home#index'
   end
@@ -6,9 +8,20 @@ RailsPrelaunchSignup::Application.routes.draw do
     root :to => "devise/registrations#new"
     match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
   end
-  devise_for :users, :controllers => { :registrations => "registrations", :confirmations => "confirmations" }
+  devise_for :users, :path => 'accounts', :controllers => { :registrations => "registrations", :confirmations => "confirmations" }
   match 'users/bulk_invite/:quantity' => 'users#bulk_invite', :via => :get, :as => :bulk_invite
   resources :users, :only => [:show, :index] do
     get 'invite', :on => :member
+
+    resources :categories
   end
+
+  resources :categories do
+    resources :tasks
+  end
+
+
+
 end
+
+
