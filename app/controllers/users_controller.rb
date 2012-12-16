ae6d9f973af7b7c7aeff35ef297e2eb21ae36da2
Jack Grossmann <jtg028@gmail.com>
2012-12-16 16:25:07 -0500

    cleaned up blank spaces

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,22 +6,21 @@ class UsersController < ApplicationController
     @users = User.all
     @chart = create_chart
   end
-  
+
   def show
     @user = User.find(params[:id])
     @category = @user.categories.new
     @categories = @user.categories.all
     @task = Task.new
-    
   end
-  
+
   def invite
     authorize! :invite, @user, :message => 'Not authorized as an administrator.'
     @user = User.find(params[:id])
     @user.send_confirmation_instructions
     redirect_to :back, :only_path => true, :notice => "Sent invitation to #{@user.email}."
   end
-  
+
   def bulk_invite
     authorize! :bulk_invite, @user, :message => 'Not authorized as an administrator.'
     users = User.where(:confirmation_token => nil).order(:created_at).limit(params[:quantity])
@@ -30,9 +29,9 @@ class UsersController < ApplicationController
     end
     redirect_to :back, :only_path => true, :notice => "Sent invitation to #{users.count} users."
   end
-  
+
   private
-  
+
   def create_chart
     users_by_day = User.group("DATE(created_at)").count
     data_table = GoogleVisualr::DataTable.new
