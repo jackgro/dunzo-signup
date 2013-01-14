f9ef722c73d59241664510ad2734450ba6e387b6
Jack Grossmann <jtg028@gmail.com>
2013-01-13 21:21:28 -0500

    Add helper method for display of date in task list, modify layout, add date, modify css

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,21 @@
+module UsersHelper
+  def date_header(date)
+    if date == Date.today
+      content_tag(:span, class: "today") do
+        "Today:"
+      end
+    elsif date == (Date.today - 1.day)
+      content_tag(:span, class: "today") do
+        "Yesterday:"
+      end
+    elsif date == (Date.today + 1.day)
+      content_tag(:span, class: "today") do
+        "Tomorrow:"
+      end
+    else
+      content_tag(:span, class: "today") do
+        "Date:"
+      end
+    end
+  end
+end
