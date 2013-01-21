b6b783af643b01d3b90d8e76dc70a60fc53728ee
Jack Grossmann <jtg028@gmail.com>
2013-01-20 23:17:49 -0500

    Add text_field date select to header

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,8 +1,5 @@
-jQuery.fn.loadOnCreate = function() {
-	this.find('a.delete-task').show();
-	return this;
-}
-
-$(function(){
-	$('#new_task').loadOnCreate();
-});
+$(document).ready(function(){
+  $('.date-select').datepicker({
+    dateFormat: 'yy-mm-dd',
+  });
+}); 
