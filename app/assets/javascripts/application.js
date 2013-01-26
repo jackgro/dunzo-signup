5097782bb70fc27b4e13f6a83c253facaa025444
Jack Grossmann <jtg028@gmail.com>
2013-01-26 18:34:35 -0500

    Only use the bootstrap js we need

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,22 +1,29 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
+// This is a manifest file that'll be  into application.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
+// the  file.
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.ui.datepicker
-//= require bootstrap
+//= require bootstrap-transition.js
+//= require bootstrap-alert.js
+//= require bootstrap-button.js
+//= require bootstrap-collapse.js
+//= require bootstrap-dropdown.js
+//= require bootstrap-modal.js
+//= require bootstrap-tab.js
+//= require bootstrap.js
 //= require dataTables/jquery.dataTables
 //= require dataTables/jquery.dataTables.bootstrap
 //= require_tree .
+
 $('document').ready(function() {
 
   // use AJAX to submit the "request invitation" form
