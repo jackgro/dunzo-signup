e1f935dfde3df0870d2613d7053eb17e10a60ebe
Jack Grossmann <jtg028@gmail.com>
2013-02-14 23:50:55 -0500

    Add home manifest js and css, add modal to homepage

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,12 @@
+//= require jquery
+//= require jquery_ujs
+//= require custom_bootstrap/bootstrap.min.js
+//= require jquery.mobile-touch-swipe-1.0.js
+//= require jquery.lazyload.min.js
+//= require jquery.epicfullscreen.js
+//= require waypoints.min.js
+//= require jquery.parallax-1.1.3.js
+//= require onepage.js
+//= require jquery.fancybox-1.3.4.pack.js
+//= require common.js
+//= require subscribe.js
