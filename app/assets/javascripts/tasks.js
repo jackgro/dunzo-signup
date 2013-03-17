52eea84c52be0fc98de95c108109c5786a45cfe7
Jack Grossmann <jtg028@gmail.com>
2013-03-17 15:59:20 -0400

    Remove old plugin

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -12,17 +12,6 @@ jQuery.fn.resetForm = function() {
   return this;
 }
 
-// Hide new task form on blur and replace with new task link
-// ====== Not using for now =============
-/*jQuery.fn.hideAndReplaceWith = function($form, $link){
-  this.blur(function(){
-    $form.fadeOut(function(){
-      $link.fadeIn('fast');
-      $(this).remove();
-    })('fast');
-  });
-}*/
-
 $(function(){
   $('.edit_task').submitOnCheck();
 });
