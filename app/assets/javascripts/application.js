7fb6e5537df2d6040719385b7d46e22814546548
Jack Grossmann <jtg028@gmail.com>
2013-03-20 22:28:01 -0400

    Remove social js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,10 +30,6 @@
 
 $('document').ready(function() {
 
-  if ($('.thankyou').length > 0) {
-    loadSocial();
-  }
-
   if (!navigator.userAgent.match(/iPhone/)) {
 
     // Override default confirm dialog with Bootbox
@@ -67,32 +63,3 @@ $('document').ready(function() {
 
   }
 });
-
-// load social sharing scripts if the page includes a Twitter "share" button
-function loadSocial() {
-
-    //Twitter
-    if (typeof (twttr) != 'undefined') {
-      twttr.widgets.load();
-    } else {
-      $.getScript('http://platform.twitter.com/widgets.js');
-    }
-
-    //Facebook
-    if (typeof (FB) != 'undefined') {
-      FB.init({ status: true, cookie: true, xfbml: true });
-    } else {
-      $.getScript("http://connect.facebook.net/en_US/all.js#xfbml=1", function () {
-        FB.init({ status: true, cookie: true, xfbml: true });
-      });
-    }
-
-    //Google+
-    if (typeof (gapi) != 'undefined') {
-      $(".g-plusone").each(function () {
-        gapi.plusone.render($(this).get(0));
-      });
-    } else {
-      $.getScript('https://apis.google.com/js/plusone.js');
-    }
-}
