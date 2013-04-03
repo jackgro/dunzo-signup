28d4e93f3174fc08b23dbfc7045ca7c020d793be
Jack Grossmann <jtg028@gmail.com>
2013-04-02 22:48:13 -0400

    Modify migration to produce SHA/'s for existing users categories

--- db/migrate/20130403023623_add_category_uid_to_categories.rb
+++ db/migrate/20130403023623_add_category_uid_to_categories.rb
@@ -1,5 +1,16 @@
 class AddCategoryUidToCategories < ActiveRecord::Migration
-  def change
+  def up
     add_column :categories, :category_uid, :string
+
+    User.find_each do |user|
+      user.categories.each do |category|
+        category.set_category_uid
+        category.save
+      end
+    end
+  end
+
+  def down
+    remove_column :categories, :category_uid
   end
 end
