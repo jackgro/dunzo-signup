9e7d0bfafc410d731927cef392392e5271cd8523
Jack Grossmann <jtg028@gmail.com>
2013-04-06 23:50:55 -0400

    Remove best_in_place

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -5,13 +5,4 @@ $(function(){
     $('div.new-user-category-form').slideToggle(200);
   });
 
-  $('.best_in_place').best_in_place();
-  $('.best_in_place').bind('ajax:error', function(){
-    bootbox.alert("Sorry! That's not a valid category name. Please try again.");
-  });
-  $('.best_in_place').bind('ajax:success', function(){
-    $url = $('#category-list').data('url');
-    $.get($url);
-  });
-
 });
