b9d8ed82f4584accb446446e0ecf206b29d0d569
Jack Grossmann <jtg028@gmail.com>
2013-04-07 21:57:58 -0400

    Modify initial data method to save category upon creation. Otherwise error after signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -92,6 +92,7 @@ class User < ActiveRecord::Base
     @category = Category.create!(name: 'Dunzo')
     @category.user_id = self.id
     @category.set_category_uid
+    @category.save
     @category.tasks.create(name: 'Make a new list', date: Date.today, position: 0)
     @category.tasks.create(name: 'Use the form in the sidebar to add one', date: Date.today, position: 1)
     @category.tasks.create(name: 'Add tasks to your new list', date: Date.today, position: 2)
