7558c7eb029ff12fe2bcd11cd9a9f4092b062739
Jack Grossmann <jtg028@gmail.com>
2013-04-13 11:34:25 -0400

    Reverse order of tasks

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,7 +4,7 @@ class Category < ActiveRecord::Base
   has_many :tasks, dependent: :destroy
 
   def tasks_by_date(date)
-    tasks.where(date: date).order('position DESC')
+    tasks.where(date: date).order('position ASC')
   end
 
   def set_category_uid
