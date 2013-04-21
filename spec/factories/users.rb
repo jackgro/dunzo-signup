6ccb7bd382402762ca9cc9134d17e43c660bd9de
Jack Grossmann <jtg028@gmail.com>
2013-04-20 20:17:13 -0400

    Add sequences to factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,12 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+
   factory :user do
     first_name 'Test'
     last_name 'User'
-    username 'testuser'
-    email 'example@example.com'
+    sequence(:username) { |n| "testuser#{n}" }
+    sequence(:email) { |n| "example#{n}@example.com" }
     password 'please'
     password_confirmation 'please'
     # required if the Devise Confirmable module is used
