fe0f5e9d13850138c12b7bc1bd2b41169669d2da
Jack Grossmann <jtg028@gmail.com>
2013-06-09 12:58:46 -0400

    Fix user factory username

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,7 +5,7 @@ FactoryGirl.define do
   factory :user do
     first_name 'Test'
     last_name 'User'
-    sequence(:username) { |n| "testuser#{n}" }
+    sequence(:username) { |n| "testuser-#{n}" }
     sequence(:email) { |n| "example#{n}@example.com" }
     password 'please'
     password_confirmation 'please'
